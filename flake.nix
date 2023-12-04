a68fec32efc0a0fb69c4081096ce21dee1a1762a
Adrian Schmitz <adrian@root.life>
2023-12-04 08:37:25 -0600

    bumped locks; moved to 23.11

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Panic's Essentials";
 
   inputs = {
-    nixpkgs-stable.url = "nixpkgs/nixos-23.05";
+    nixpkgs-stable.url = "nixpkgs/nixos-23.11";
     flake-utils = { url = "github:numtide/flake-utils"; };
     rust-overlay = { url = "github:oxalica/rust-overlay"; };
     pri-templates.url = "github:panicmacc/templates";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-stable";
   };
 
